397b4a3a67ef238e737f87f716f9815c630672f9
Prathyusha Goparaju <prathyushagoparaju11@gmail.com>
2020-07-22 21:57:37 +0900

    Add files via upload

---
+++ std_Mng_Project.py
@@ -0,0 +1,119 @@
+class Student:
+    #constructor
+    def __int__(self,name,age,roll no,branch):
+        
+        self.name = name
+        self.age = age
+        self.roll no = roll no
+        self.branch = branch
+        
+    # Function to add new student
+    
+    def addStudent():
+        new = Student(self,name,age,roll no,branch,GPA = 0)
+        
+    # add to the students list
+        ls.append(new)
+    
+    #function to update student bio
+    
+    def bioUpdate(self,address,phone number):
+        
+        self.address = address
+        
+        self.phone number = phone number
+        
+         i = int(input(" enter student roll no "))
+            
+     # print name and roll no of the student to verify the correct match or not       
+            
+            print("name: ",new.name)
+            
+            print("roll no: ", new.roll no)
+            
+      # add address and phone number
+    
+            address = input(" enter student address ")
+            
+                phone number = input(" enter 10 digits phone number ")
+            
+                    print( "address: ", new.address)
+            
+                    print("phone number: ", new.phone number)
+            
+                    print("\n")
+            
+            
+        # function to update the score
+        
+         def scoreUpdate(self,m1,m2,m3,m4,m5):
+                
+                self.marks1 = m1
+                self.marks2 = m2
+                self.marks3 = m3
+                self.marks4 = m4
+                self.marks5 = m5
+                
+                new_scores = input('m1','m2','m3','m4','m5')
+                
+                print("new_scores")
+                
+                
+       # function to delete a student
+    
+                def rustricate(self,roll no):
+                
+                #for i in rage(ls()):
+                    
+                   # if (ls[i].roll no == remove)
+                    
+                   # return i
+                
+                del ls[i]
+                
+       # function to graduate students
+                
+        
+                def graduate():
+                
+                print("enter marks obtained: ")
+                
+                m1 = int(m1);
+                m2 = int(m2);
+                m3 = int(m3);
+                m4 = int(m4);
+                m5 = int(m5);
+                
+                sum = m1+m2+m3+m4+m5;
+                
+                average = sum/5
+                
+                if (average >= 80):
+                    
+                    print('Grade A');
+                    
+                    elif(average >= 60):
+                        
+                        print('Grade B');
+                        
+                        elif(average >= 50):
+                            
+                            print('Grade C');
+                            
+                            elif(average <= 40):
+                                
+                                print('Fail');
+                                
+                                else:
+                                    print("Roll no not found");
+                                
+                 
+                                
+                    
+            
+         
+         
+            
+        
+        
+        
